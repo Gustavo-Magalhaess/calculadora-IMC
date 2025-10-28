18a5d6185925d3ac69e4e52358f2e91381bb0bae
Gustavopkl <gusttavohr6@gmail.com>
2025-10-28 16:51:56 -0300

    alteracao

--- src/Main.java
+++ src/Main.java
@@ -19,19 +19,25 @@ public class Main {
 
         if (imc < 18.5) {
 
-            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + "Abaixo do peso");
+            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + " Abaixo do peso");
 
         } else if (imc >= 18.6 && imc < 24.9) {
 
-            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + "Peso ideal");
+            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + " Peso ideal");
 
         }else if(imc >= 25.0 && imc <= 29.9){
 
-            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + "Você está acima do peso");
+            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + " Você está acima do peso");
 
         } else if(imc >= 30 && imc <= 34.9){
 
-            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + "Obesidade grau 1");
+            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + " Obesidade grau 1");
+        } else if(imc >= 35 && imc <= 34.9){
+
+            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + " Obesidade grau 2 (Severa)");
+        } else if (imc >= 40){
+
+            JOptionPane.showMessageDialog(null, "O IMC do usuário é " + formatado + " Obesidade grau 3 (Morbida)");
         }
 
         sc.close();
